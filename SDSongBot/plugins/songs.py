3285b044b740295b93b8b27cd9736b5fe3e70c7d
King Amda <86409667+King-Amda@users.noreply.github.com>
2021-10-07 09:46:05 +0530

    King Amda

--- SDSongBot/plugins/songs.py
+++ SDSongBot/plugins/songs.py
@@ -48,7 +48,7 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("📥 Downloading the song by @SDbotsz 📥")
+    m.edit("📥 Downloading the song by @ImSaantha 📥")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
